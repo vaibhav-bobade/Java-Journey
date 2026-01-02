175761ac1deb7979109660d872ca64a69bd201f7
vaibhav <vaibhavabobade@gmail.com>
2026-01-02 15:44:30 +0530

    Added Variable Practice Code and Fixed gitignore

---
+++ Java Practice/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Printing my Name
+        String name = "Vaibhav";
+        //Printing my Age
+        int age = 19;
+        //Is Student True or False
+        boolean isstudent = true;
+        //Fourth Semister CGPA
+        double gpa = 7.75;
+
+        //Printing values to Console
+        System.out.println("Name :" + name);
+        System.out.println("Age :" + age);
+        System.out.println("Is Student? :" + isstudent);
+        System.out.println("Fourth Semister CGPA :" + gpa);
+    }
+}
