5e398f290b2ea070c8e50b04842f026cc94f2240
vaibhav <vaibhavabobade@gmail.com>
2026-01-06 23:42:53 +0530

    Increment and Decrement Operators

---
+++ Java Practice/InceDecrement.java
@@ -0,0 +1,11 @@
+public class InceDecrement {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = ++b;
+
+        System.out.println(c);
+        System.out.println(c + a);
+        System.out.println(b);
+    }
+}
