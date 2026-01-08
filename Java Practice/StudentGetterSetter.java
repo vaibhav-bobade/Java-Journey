7c489fb5ec1c5007f50b32a6fecd909bf2f04f6b
vaibhav <vaibhavabobade@gmail.com>
2026-01-08 19:00:49 +0530

    Getter Setter code for Student Info

---
+++ Java Practice/StudentGetterSetter.java
@@ -0,0 +1,42 @@
+ class StudentGetSet {
+    private String name;
+    private int rollNo;
+    private int marks;
+
+    public void setName(String n){
+        name = n;
+    }
+    public void setRollNO(int roll){
+        rollNo = roll;
+    }
+    public void setMarks(int m){
+        if(m>=0){
+            marks = m;
+        }
+        else{
+            System.out.println("Enter Valid Marks..");
+        }
+    }
+    public String getName(){
+        return name;
+    }
+    public int getRollNo(){
+        return rollNo;
+    }
+    public int getMarks(){
+        return marks;
+    }
+}
+public class StudentGetterSetter{
+    public static void main(String[] args) {
+        StudentGetSet st = new StudentGetSet();
+
+        st.setName("Vaibhav");
+        st.setRollNO(21);
+        st.setMarks(75);
+
+        System.out.println("Student name: " + st.getName());
+        System.out.println("Student Roll No.: " + st.getRollNo());
+        System.out.println("Student Marks: " + st.getMarks());
+    }
+}
