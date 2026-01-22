43f966ee7773c941882c06bbb2323d0176275825
vaibhav <vaibhavabobade@gmail.com>
2026-01-22 22:59:08 +0530

    Remove Duplicates from sorted Array

---
+++ Java DSA/RemoveDuplicatesLC26.java
@@ -0,0 +1,29 @@
+public class RemoveDuplicatesLC26{
+    public static int removeDuplicates(int[] nums) {
+        int left = 0;
+        for(int right=1; right<nums.length; right++){
+            while(nums[left] != nums[right]){
+                left++;
+                nums[left] = nums[right];
+            }
+            
+        }
+        return left+1;
+    }
+    public static void main(String[] args) {
+       int[] nums = {0,0,1,1,1,2,2,3,3,4};
+       System.out.println(removeDuplicates(nums));
+    }
+}
+
+// Example 1:---------------------------
+// Input: nums = [1,1,2]
+// Output: 2, nums = [1,2,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+// Example 2:-----------------------------
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
